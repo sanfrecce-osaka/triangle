4fb8f26b8e31f5b9bc44abd1996d3f0e4d79c970
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2017-05-19 16:56:12 +0900

    求められる形が三角形でない場合の挙動を実装

--- triangle.rb
+++ triangle.rb
@@ -1,13 +1,18 @@
 class Triangle
   class << self
     def display_shape(line_a, line_b, line_c)
-      '辺の長さは0より大きい値を入力してください！' if or_less_zero?(line_a, line_b, line_c)
+      return '辺の長さは0より大きい値を入力してください！' if or_less_zero?(line_a, line_b, line_c)
+      '三角形じゃないです＞＜' unless triangle?(line_a, line_b, line_c)
     end
 
     private
 
     def or_less_zero?(line_a, line_b, line_c)
       line_a <= 0 || line_b <= 0 || line_c <= 0
+    end
+
+    def triangle?(line_a, line_b, line_c)
+      line_a + line_b > line_c && line_b + line_c > line_a && line_c + line_a > line_b
     end
   end
 end
