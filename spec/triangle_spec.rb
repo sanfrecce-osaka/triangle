d18903b35f239dd2b29b990ecaa6dca60f66463e
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2017-05-19 17:11:25 +0900

    3辺のうち2辺の長さが同一の場合のテストケースを追加

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -2,8 +2,16 @@ require File.expand_path(File.dirname(__FILE__) + '/../triangle')
 
 describe Triangle do
   context '与えられた辺の長さが全て0より大きい場合' do
-    context '3辺の長さが全て同一の場合' do
-      it { expect(Triangle.display_shape(1, 1, 1)).to eq '正三角形ですね！' }
+    context '2辺の長さの合計が残りの1辺の長さより大きい場合' do
+      context '3辺の長さが全て同一の場合' do
+        it { expect(Triangle.display_shape(1, 1, 1)).to eq '正三角形ですね！' }
+      end
+
+      context '3辺のうち2辺の長さが同一の場合' do
+        it { expect(Triangle.display_shape(1, 2, 2)).to eq '二等辺三角形ですね！' }
+        it { expect(Triangle.display_shape(2, 1, 2)).to eq '二等辺三角形ですね！' }
+        it { expect(Triangle.display_shape(2, 2, 1)).to eq '二等辺三角形ですね！' }
+      end
     end
 
     context '2辺の長さの合計が残りの1辺の長さ以下の場合' do
