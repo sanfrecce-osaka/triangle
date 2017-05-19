d7f12e983886b03954a355979fcf9c297e3d73c5
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2017-05-19 17:08:09 +0900

    3辺の長さが全て同一の場合の挙動を実装

--- triangle.rb
+++ triangle.rb
@@ -2,7 +2,11 @@ class Triangle
   class << self
     def display_shape(line_a, line_b, line_c)
       return '辺の長さは0より大きい値を入力してください！' if or_less_zero?(line_a, line_b, line_c)
-      '三角形じゃないです＞＜' unless triangle?(line_a, line_b, line_c)
+      return '三角形じゃないです＞＜' unless triangle?(line_a, line_b, line_c)
+
+      if equilateral?(line_a, line_b, line_c)
+        '正三角形ですね！'
+      end
     end
 
     private
@@ -14,5 +18,10 @@ class Triangle
     def triangle?(line_a, line_b, line_c)
       line_a + line_b > line_c && line_b + line_c > line_a && line_c + line_a > line_b
     end
+
+    def equilateral?(line_a, line_b, line_c)
+      line_a == line_b && line_b == line_c
+    end
   end
 end
+
