64397fbde415cdb718192dc5295d7c52f165109a
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2017-05-19 16:57:18 +0900

    3辺の長さが全て同一の場合のテストケースを追加

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -2,9 +2,15 @@ require File.expand_path(File.dirname(__FILE__) + '/../triangle')
 
 describe Triangle do
   context '与えられた辺の長さが全て0より大きい場合' do
-    it { expect(Triangle.display_shape(1, 2, 3)).to eq '三角形じゃないです＞＜' }
-    it { expect(Triangle.display_shape(3, 1, 2)).to eq '三角形じゃないです＞＜' }
-    it { expect(Triangle.display_shape(2, 3, 1)).to eq '三角形じゃないです＞＜' }
+    context '3辺の長さが全て同一の場合' do
+      it { expect(Triangle.display_shape(1, 1, 1)).to eq '正三角形ですね！' }
+    end
+
+    context '2辺の長さの合計が残りの1辺の長さ以下の場合' do
+      it { expect(Triangle.display_shape(1, 2, 3)).to eq '三角形じゃないです＞＜' }
+      it { expect(Triangle.display_shape(3, 1, 2)).to eq '三角形じゃないです＞＜' }
+      it { expect(Triangle.display_shape(2, 3, 1)).to eq '三角形じゃないです＞＜' }
+    end
   end
 
   context '与えられた辺の長さに0以下が含まれている場合' do
