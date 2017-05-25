59bf306e4d5683b048e7d2d7110b908c80ff875d
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2017-05-25 13:43:46 +0900

    リファクタリング

--- triangle.rb
+++ triangle.rb
@@ -7,13 +7,12 @@ class Triangle
     def display_shape(line_a, line_b, line_c)
       lines = [line_a, line_b, line_c].map { |line| NKF.nkf('-m0Z1 -W -w', line).to_r }
 
-      # 引数に整数、小数、分数以外が指定された場合String#to_rで0/1に変換されるため、ここでメッセージを表示する
-      return '辺の長さは0より大きい整数、小数、分数を半角または全角で入力してください！' if or_less_zero?(lines)
+      return '辺の長さは0より大きい整数、小数、分数を半角または全角で入力してください！' if zero_or_negative?(lines)
       return '三角形じゃないです＞＜' unless triangle?(lines)
 
-      if equilateral?(lines)
+      if equal_all_lengths?(lines)
         '正三角形ですね！'
-      elsif isosceles?(lines)
+      elsif equal_two_lengths?(lines)
         '二等辺三角形ですね！'
       else
         '不等辺三角形ですね！'
@@ -22,20 +21,21 @@ class Triangle
 
     private
 
-    def or_less_zero?(lines)
-      (lines[0] <=> 0) <= 0 || (lines[1] <=> 0) <= 0 || (lines[2] <=> 0) <= 0
+    def zero_or_negative?(lines)
+      # 別の書き方 !lines.all?(&:positive?)
+      lines.any? { |line| line <= 0 }
     end
 
     def triangle?(lines)
-      (lines[0] + lines[1] <=> lines[2]) == 1 && (lines[1] + lines[2] <=> lines[0]) == 1 && (lines[2] + lines[0] <=> lines[1]) == 1
+      (lines[0] + lines[1] > lines[2]) && (lines[1] + lines[2] > lines[0]) && (lines[2] + lines[0] > lines[1])
     end
 
-    def equilateral?(lines)
-      lines[0] == lines[1] && lines[1] == lines[2]
+    def equal_all_lengths?(lines)
+      lines.uniq.size == 1
     end
 
-    def isosceles?(lines)
-      lines[0] == lines[1] || lines[1] == lines[2] || lines[2] == lines[0]
+    def equal_two_lengths?(lines)
+      lines.uniq.size <= 2
     end
   end
 end
